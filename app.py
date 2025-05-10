bc6b3c7faa7aca1589916f22097d2b7d5d73ede7
ritik902 <ritik004sharma@gmail.com>
2025-05-10 15:02:42 +0530

    bug fix

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ uploaded_file = st.file_uploader("Choose an image...", type="jpg")
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption='Uploaded Image.', use_column_width=True)
+    st.image(image, caption='Uploaded Image.', use_container_width=True)
 
     # Extract image features
     features = extract_features(image)
